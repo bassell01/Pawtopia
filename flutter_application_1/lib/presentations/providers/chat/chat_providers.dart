776ad5e2b2d7ebe7e27dea110836b25ab6ef7d55
Mostafahayssam <mostafahayssam58@gmail.com>
2025-12-19 15:15:43 +0200

    Fixing chat provider

--- flutter_application_1/lib/presentations/providers/chat/chat_providers.dart
+++ flutter_application_1/lib/presentations/providers/chat/chat_providers.dart
@@ -1,23 +1,45 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/di/injection_container.dart';
+import '../../../core/services/firebase_firestore_service.dart';
+
 import '../../../data/datasources/chat/chat_remote_data_source.dart';
 import '../../../data/repositories_impl/chat_repository_impl.dart';
 import '../../../domain/repositories/chat_repository.dart';
+
 import '../../../domain/usecases/chat/create_thread_if_needed.dart';
 import '../../../domain/usecases/chat/get_messages_stream.dart';
 import '../../../domain/usecases/chat/get_user_threads.dart';
 import '../../../domain/usecases/chat/send_message.dart';
-import '../core/firebase_providers.dart';
 
+/// Core service (wrapper)
+final firestoreServiceProvider = Provider<FirebaseFirestoreService>((ref) {
+  return sl<FirebaseFirestoreService>();
+});
+
+/// DataSource
 final chatRemoteDataSourceProvider = Provider<ChatRemoteDataSource>((ref) {
   return ChatRemoteDataSource(ref.read(firestoreServiceProvider));
 });
 
+/// Repository
 final chatRepositoryProvider = Provider<ChatRepository>((ref) {
   return ChatRepositoryImpl(ref.read(chatRemoteDataSourceProvider));
 });
 
-final getUserThreadsProvider = Provider((ref) => GetUserThreads(ref.read(chatRepositoryProvider)));
-final getMessagesStreamProvider = Provider((ref) => GetMessagesStream(ref.read(chatRepositoryProvider)));
-final createThreadIfNeededProvider = Provider((ref) => CreateThreadIfNeeded(ref.read(chatRepositoryProvider)));
-final sendMessageProvider = Provider((ref) => SendMessage(ref.read(chatRepositoryProvider)));
+/// UseCases
+final getUserThreadsProvider = Provider<GetUserThreads>((ref) {
+  return GetUserThreads(ref.read(chatRepositoryProvider));
+});
+
+final getMessagesStreamProvider = Provider<GetMessagesStream>((ref) {
+  return GetMessagesStream(ref.read(chatRepositoryProvider));
+});
+
+final createThreadIfNeededProvider = Provider<CreateThreadIfNeeded>((ref) {
+  return CreateThreadIfNeeded(ref.read(chatRepositoryProvider));
+});
+
+final sendMessageProvider = Provider<SendMessage>((ref) {
+  return SendMessage(ref.read(chatRepositoryProvider));
+});
